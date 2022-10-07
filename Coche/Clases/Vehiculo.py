9f639db90075cf8778d5142f80151ba014912b2b
Pelayo <phuermij@myuax.com>
2022-10-07 17:21:48 +0200

    cambios

--- Coche/Clases/Vehiculo.py
+++ Coche/Clases/Vehiculo.py
@@ -8,3 +8,19 @@ class Vehiculo():
 def __str__(self):
     
     return "Color {}, {} ruedas".format( self.color, self.ruedas )
+
+
+
+class Coche(Vehiculo):
+
+    def __init__(self, color, ruedas, velocidad, cilindrada): 
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    print("Coche creado con éxito.")
+
+def __str__(self):
+    return "color {}, {} km/h, {} ruedas, {} cc".format(self.color, self.velocidad, self.ruedas, self.cilindrada )
+
+coche = Coche("azul", 150, 4, 1200)
+print(coche.__str__())
