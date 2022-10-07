fd7fa3ea1f1f0c78f8f2a7df25e1981014c9139d
Pelayo <phuermij@myuax.com>
2022-10-07 17:21:59 +0200

    cambios

--- Ejercicio4/Errores/ejercicio4.py
+++ Ejercicio4/Errores/ejercicio4.py
@@ -1,4 +1,12 @@
 def error1():
     a = 7/0
+    try:
+        print(a)
+    except:
+        ZeroDivisionError("No se puede dividir por cero.")
+    else:
+        print("Todo correcto.")
+
+error1()
     
     
