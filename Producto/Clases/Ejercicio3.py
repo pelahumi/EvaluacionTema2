65ddd55a930071cd0d180651d1ed85db5c1c0f02
Pelayo <phuermij@myuax.com>
2022-10-07 16:36:40 +0200

    clase producto y constructor

---
+++ Producto/Clases/Ejercicio3.py
@@ -0,0 +1,10 @@
+class Producto():
+
+    def __init__(self, codigo, nombre, precio, tipo):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo = tipo
+    print("El producto se ha creado con éxito.")
+
+    
