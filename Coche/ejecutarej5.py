adc22c6839181dd35e4382fd502a660a60b27c64
Pelayo <phuermij@myuax.com>
2022-10-07 17:26:46 +0200

    errores

--- Coche/ejecutarej5.py
+++ Coche/ejecutarej5.py
@@ -4,7 +4,7 @@ def ejecutar3():
     print("Vamos a crear un coche: ")
     color = input("Introduce el color del coche: ")
     ruedas = input("Introduce el número de ruedas: ")
-    velocidad = inputç("Introduce la velocidad máxima: ")
+    velocidad = input("Introduce la velocidad máxima: ")
     cilindrada = input("Introduce la cilindrada: ")
 
     coche = Coche(color, ruedas, velocidad, cilindrada)
