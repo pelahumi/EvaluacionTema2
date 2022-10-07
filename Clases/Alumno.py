b14749e68247123d6802e945688bc151b7daa856
Pelayo <phuermij@myuax.com>
2022-10-07 15:43:51 +0200

    metodo calificacion

--- Clases/Alumno.py
+++ Clases/Alumno.py
@@ -6,3 +6,13 @@ class Alumno():
         self.nombre = nombre
         self.nota = nota
         print("El alumno se ha creado con éxito.")
+    
+    #Creamos el método de calificación
+    def calificacion(nota):
+        nota = int(input("Introduce una nota del 0 al 10"))
+        if 5 <= nota <= 10:
+            print("Has aprobado")
+        elif 0 <= nota <5:
+            print("Has suspendido")
+        else:
+            print("La nota introducida no está dentro de los parámetros")
