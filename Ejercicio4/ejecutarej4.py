dd9c0dce6794d42c89ca378b1438824a44ad8fb7
Pelayo <phuermij@myuax.com>
2022-10-07 16:54:51 +0200

    ejercicio4

---
+++ Ejercicio4/ejecutarej4.py
@@ -0,0 +1,14 @@
+def ejecutar3():
+    while True:
+
+        print("========================") 
+        print(" BIENVENIDO AL MENU DEL EJERCICIO 4") 
+        print("========================") 
+        print("[1] Error: numero  = 7/0 ")  
+        print("[2] Error:  ")
+        print("========================")
+
+        opcion = input("> ")
+
+        if opcion == 1:
+            
