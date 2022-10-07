de9bb2f4f460e38d25056865bfa445e006fbb272
Pelayo <phuermij@myuax.com>
2022-10-07 16:38:22 +0200

    metodo str

--- Producto/Clases/Ejercicio3.py
+++ Producto/Clases/Ejercicio3.py
@@ -7,4 +7,6 @@ class Producto():
         self.tipo = tipo
     print("El producto se ha creado con éxito.")
 
-    
+    def __str__(self):
+        return f"{self.codigo} {self.nombre} {self.precio} {self.tipo}"
+        
